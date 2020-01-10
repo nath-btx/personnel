22ffc77c1bec08b5963cbc7da27718e5365d616d
Feiv7 <benjamingoncalves@hotmail.fr>
2020-01-10 11:40:02 +0100

    Ajout TestEmploye

---
+++ Personnel/src/testsUnitaires/TestEmploye.java
@@ -0,0 +1,84 @@
+package testsUnitaires;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestEmploye {
+
+	@Test
+	void testEmploye() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstAdmin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstRoot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPrenom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPrenom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLigue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}

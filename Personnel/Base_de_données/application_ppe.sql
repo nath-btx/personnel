58c6b0ce4c8a2f2001d7c3b58c3cc0885feb69f1
nath-btx <nathan.bertiaux@hotmail.fr>
2020-02-28 10:21:09 +0100

    base de donnée

--- Personnel/Base_de_données/application_ppe.sql
+++ Personnel/Base_de_données/application_ppe.sql
@@ -1,77 +1,46 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 03 fév. 2020 à 13:16
--- Version du serveur :  5.7.24
--- Version de PHP :  7.2.14
-
+-- Généré le :  ven. 28 fév. 2020 à 08:11
+-- Version du serveur :  5.7.23
+-- Version de PHP :  7.2.10
+ 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
+ 
+ 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
+ 
 --
 -- Base de données :  `application_ppe`
 --
-
+ 
 -- --------------------------------------------------------
-
+ 
 --
 -- Structure de la table `appartient`
 --
-
+ 
 DROP TABLE IF EXISTS `appartient`;
 CREATE TABLE IF NOT EXISTS `appartient` (
   `IdNum` int(11) NOT NULL,
   `IdLigue` int(11) NOT NULL,
   PRIMARY KEY (`IdNum`,`IdLigue`),
-  KEY `IdLigue` (`IdLigue`)
+  KEY `IdLigue` (`IdLigue`),
+  KEY `IdNum` (`IdNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `employe`
---
-
-DROP TABLE IF EXISTS `employe`;
-CREATE TABLE IF NOT EXISTS `employe` (
-  `IdEmploye` int(255) NOT NULL AUTO_INCREMENT,
-  `Nom` varchar(255) NOT NULL,
-  `Prenom` varchar(255) NOT NULL,
-  `Mail` varchar(255) NOT NULL,
-  `Mdp` varchar(255) NOT NULL,
-  `DateArrive` date NOT NULL,
-  `DateDepart` date DEFAULT NULL,
-  `Level` int(255) NOT NULL,
-  PRIMARY KEY (`IdEmploye`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `ligue`
---
-
-DROP TABLE IF EXISTS `ligue`;
-CREATE TABLE IF NOT EXISTS `ligue` (
-  `IdLigue` int(255) NOT NULL AUTO_INCREMENT,
-  `NomLigue` varchar(255) NOT NULL,
-  PRIMARY KEY (`IdLigue`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+ 
 --
 -- Contraintes pour les tables déchargées
 --
-
+ 
 --
 -- Contraintes pour la table `appartient`
 --
@@ -79,7 +48,7 @@ ALTER TABLE `appartient`
   ADD CONSTRAINT `appartient_ibfk_1` FOREIGN KEY (`IdLigue`) REFERENCES `ligue` (`IdLigue`),
   ADD CONSTRAINT `appartient_ibfk_2` FOREIGN KEY (`IdNum`) REFERENCES `employe` (`IdEmploye`);
 COMMIT;
-
+ 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

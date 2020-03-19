66147c15418635106c66efc39cbe58fd1667112d
nab98 <55541995+nab98@users.noreply.github.com>
2020-03-19 11:09:42 +0100

    2eme etape jbdc insertion

--- Personnel/src/databasesConnection.java
+++ Personnel/src/databasesConnection.java
@@ -2,30 +2,52 @@
 	import java.sql.DriverManager;
 	import java.sql.Statement;
 	import java.sql.SQLException;
-	
+	import java.sql.ResultSet;
 	
 	
 	public class databasesConnection {
-	   public static void main(String[] args) {
+	   public static void main(String[] args) throws SQLException {
 
-	      String JdbcURL = "jdbc:mysql://localhost:3306/application_ppe";
+	      String JdbcURL = "jdbc:mysql://localhost:3306/application_ppe?useUnicode=true & useJDBCCompliantTimezoneShift=true & useLegacyDatetimeCode = false & serverTimezone=UTC";
 	      String Username = "root";
 	      String password = "";
 	      //probleme fuseau horaire avec W10 il faut changer la timezone de mysql avec SET @@global.time_zone = '+00:00'; et
 	      // SET @@session.time_zone = '+00:00';
 	      //puis SELECT @@global.time_zone, @@session.time_zone; pour verifier que la time zone a bien etait modifié
-	      Connection connection;
+	      Connection connection = null;
+	      
+	      Statement myStmt = null;
 	      try {
 	         System.out.println("Connecting to database..............."+JdbcURL);
 	         Class.forName("com.mysql.cj.jdbc.Driver");
 	         connection = DriverManager.getConnection(JdbcURL, Username, password);
 	         System.out.println("Connection is successful!!!!!!");
+	         
+	         // 2. Create a statement
+	            myStmt = connection.createStatement();
+	 
+	            // 3. Execute SQL query
+	            String sql = "insert into employe " + " (IdEmploye, Nom, Prenom, Mail, Mdp, DateArrive, DateDepart, Level)"
+	                    + " values ('2', 'test','toto','test@test.com','test','2020-03-19','2020-04-19','0')";
+	 
+	            myStmt.executeUpdate(sql);
+	            
+	            System.out.println("Insert complete.");
+	 
 	      }
 	      catch (ClassNotFoundException e) {
 	            e.printStackTrace();
 	      }
 	      catch(SQLException e) {
 	         e.printStackTrace();
-	      }
+	      }finally {
+	            if (myStmt != null) {
+	                myStmt.close();
+	            }
+	 
+	            if (connection != null) {
+	                connection.close();
+	            }
 	   }
+	}
 	}

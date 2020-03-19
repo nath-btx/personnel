d1e9b6606b89c384b14511e1ebd5305aafb3c3da
nath-btx <nathan.bertiaux@hotmail.fr>
2020-03-19 12:05:12 +0100

    BDD

--- Personnel/src/personnel/JBDC.java
+++ Personnel/src/personnel/JBDC.java
@@ -20,8 +20,9 @@ package personnel;
 	      
 	      Statement myStmt = null;
 	      try {
-	         System.out.println("Connecting to database..............."+JdbcURL);
-	         Class.forName("com.mysql.cj.jdbc.Driver");
+	         System.out.println("Connecting to database...............");
+	         System.out.println(JdbcURL);
+	         Class.forName("com.mysql.cj.jdbc.Driver"); 
 	         connection = DriverManager.getConnection(JdbcURL, Username, password);
 	         System.out.println("Connection is successful!!!!!!");
 	         
@@ -29,8 +30,8 @@ package personnel;
 	            myStmt = connection.createStatement();
 	 
 	            // 3. Execute SQL query
-	            String sql = "insert into employe " + " (IdEmploye, Nom, Prenom, Mail, Mdp, DateArrive, DateDepart, Level)"
-	                    + " values ('0', 'test','toto','test@test.com','test','2020-03-19','2020-04-19','0')";
+	            String sql = "insert into employe " + " (Nom, Prenom, Mail, Mdp, DateArrive, DateDepart, Level)"
+	                    + " values ('test','toto','test@test.com','test','2020-03-19','2020-04-19','0')";
 	 
 	            myStmt.executeUpdate(sql);
 	            

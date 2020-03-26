e6ce33ade4a2992ef1d86e2c0abd49fa29420e4f
Feiv7 <benjamingoncalves@hotmail.fr>
2020-03-26 19:40:22 +0100

    JdbcURL simplifier

--- Personnel/src/personnel/JBDC.java
+++ Personnel/src/personnel/JBDC.java
@@ -11,64 +11,64 @@ import java.sql.SQLException;
 	 public class JBDC implements Passerelle  {
 	   public static void main(String[] args) throws SQLException {
 
-//	      String JdbcURL = "jdbc:mysql://localhost:3306/application_ppe?useUnicode=true & useJDBCCompliantTimezoneShift=true & useLegacyDatetimeCode = false & serverTimezone=UTC";
-//	      String Username = "root";
-//	      String password = "";
-//	      Connection connection = null;
-//	      
-//	      Statement myStmt = null;
-//	      try {
-//	         System.out.println("Connecting to database...............");
-//	         System.out.println(JdbcURL);
-//	         Class.forName("com.mysql.cj.jdbc.Driver"); 
-//	         connection = DriverManager.getConnection(JdbcURL, Username, password);
-//	         System.out.println("Connection is successful!!!!!!");
-//	         
-//	         // 2. Create a statement
-//	            myStmt = connection.createStatement();
-//	 
-//	            // 3. Execute SQL query
-//	            String sql = "insert into employe " + " (Nom, Prenom, Mail, Mdp, DateArrive, DateDepart, Level)";
-//	            //insert
-//	            String insert = "insert into employe " + " (Nom, Prenom, Mail, Mdp, DateArrive, DateDepart, Level)"
-//	                    + " values ('test','toto','test@test.com','test','2020-03-19','2020-04-19','0')";
-//	 
-//	            myStmt.executeUpdate(insert);
+	      String JdbcURL = "jdbc:mysql://localhost:3306/application_ppe?";
+	      String Username = "root";
+	      String password = "";
+	      Connection connection = null;
+	      
+	      Statement myStmt = null;
+	      try {
+	         System.out.println("Connecting to database...............");
+	         System.out.println(JdbcURL);
+	         Class.forName("com.mysql.cj.jdbc.Driver"); 
+	         connection = DriverManager.getConnection(JdbcURL, Username, password);
+	         System.out.println("Connection is successful!!!!!!");
+	         
+	         // 2. Create a statement
+	            myStmt = connection.createStatement();
+	 
+	            // 3. Execute SQL query
+	            String sql = "insert into employe " + " (Nom, Prenom, Mail, Mdp, DateArrive, DateDepart, Level)";
+	            //insert
+	            String insert = "insert into employe " + " (Nom, Prenom, Mail, Mdp, DateArrive, DateDepart, Level)"
+	                    + " values ('test','toto','test@test.com','test','2020-03-19','2020-04-19','0')";
+	 
+	            myStmt.executeUpdate(insert);
+	            
+	            System.out.println("Insert complete.");
+	            
+	            //update
+//	            String update = "update employe set Mail='demo@test.com' where nom='toto'";
 //	            
-//	            System.out.println("Insert complete.");
-//	            
-//	            //update
-////	            String update = "update employe set Mail='demo@test.com' where nom='toto'";
-////	            
-////	            int rowsAffected = myStmt.executeUpdate(update);
-////	 
-////	            System.out.println("Rows affected: " + rowsAffected);
-////	            System.out.println("Update complete.");
-//	            
-//	            //delete
-//	            
-////	            String delete = "delete from employe where nom='toto'";
-////	            
-////	            int rowsAffected1 = myStmt.executeUpdate(delete);
-////	 
-////	            System.out.println("Rows affected: " + rowsAffected1);
-////	            System.out.println("Delete complete.");
+//	            int rowsAffected = myStmt.executeUpdate(update);
 //	 
-//	      }
-//	      catch (ClassNotFoundException e) {
-//	            e.printStackTrace();
-//	      }
-//	      catch(SQLException e) {
-//	         e.printStackTrace();
-//	      }finally {
-//	            if (myStmt != null) {
-//	                myStmt.close();
-//	            }
+//	            System.out.println("Rows affected: " + rowsAffected);
+//	            System.out.println("Update complete.");
+	            
+	            //delete
+	            
+//	            String delete = "delete from employe where nom='toto'";
+//	            
+//	            int rowsAffected1 = myStmt.executeUpdate(delete);
 //	 
-//	            if (connection != null) {
-//	                connection.close();
-//	            }
-//	   }
+//	            System.out.println("Rows affected: " + rowsAffected1);
+//	            System.out.println("Delete complete.");
+	 
+	      }
+	      catch (ClassNotFoundException e) {
+	            e.printStackTrace();
+	      }
+	      catch(SQLException e) {
+	         e.printStackTrace();
+	      }finally {
+	            if (myStmt != null) {
+	                myStmt.close();
+	            }
+	 
+	            if (connection != null) {
+	                connection.close();
+	            }
+	   }
 	}
 
 	@Override
@@ -87,7 +87,7 @@ import java.sql.SQLException;
 	
 	public static void enregistrerEmploye(Employe employe) throws SQLException
 	{
-		 String JdbcURL = "jdbc:mysql://localhost:3306/application_ppe?useUnicode=true & useJDBCCompliantTimezoneShift=true & useLegacyDatetimeCode = false & serverTimezone=UTC";
+		 String JdbcURL = "jdbc:mysql://localhost:3306/application_ppe?";
 	      String Username = "root";
 	      String password = "";
 	      Connection connection = null;
@@ -150,7 +150,7 @@ import java.sql.SQLException;
 	
 	public void enregistrerLigue(Ligue ligue) throws SQLException
 	{
-		 String JdbcURL = "jdbc:mysql://localhost:3306/application_ppe?useUnicode=true & useJDBCCompliantTimezoneShift=true & useLegacyDatetimeCode = false & serverTimezone=UTC";
+		 String JdbcURL = "jdbc:mysql://localhost:3306/application_ppe?";
 	      String Username = "root";
 	      String password = "";
 	      Connection connection = null;
